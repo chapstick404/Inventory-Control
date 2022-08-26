64200bdeb2dcfc3af9479890fb07b3c2fe3cbc42
chapstick404 <null.nullth@gmail.com>
2022-08-26 09:51:44 -0600

    CursesDisplay can now display widgets

--- CursesDisplay.py
+++ CursesDisplay.py
@@ -5,10 +5,28 @@ class DisplayWidget: #basic display widget
         self.value = value
         self.id = id
         self.onClose = onClose
+        self.widgets = [self]
 
     def draw(self, win: curses.window): #basic class to implement the function
-        win.addstr(self.value)
-        win.refresh()
+        for widget in self.widgets:
+            if widget is self:
+                win.addstr(self.value)
+            else:
+                widget.draw(win)
+
+    def add_widget(self, widget):
+        self.widget.append(widget)
+
+class TitleWidget(DisplayWidget):
+    def __int__(self, title: str, onClose=None, pos=None):
+        super.__init__(self, title, onClose)
+        if not pos:
+            pos = "Top-Left"
+
+    def draw(self, win: curses.window):
+        if self.pos == "Top-left":
+            win.addstr(self.value)
+
 class CursesDisplay: # a container to connect window and widgets together
     def __init__(self, scrn: curses.window):
         self.widgets = []
